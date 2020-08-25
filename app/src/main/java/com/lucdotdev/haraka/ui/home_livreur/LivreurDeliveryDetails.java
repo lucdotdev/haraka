10ab592e43a496e1b50711ba291bea5a308aa4a7
luc-dotcom <lucm.dotcom@gmail.com>
2020-08-25 22:58:57 +0200

    [v.1.0.0] FIX MAJOR BUGS

---
+++ app/src/main/java/com/lucdotdev/haraka/ui/home_livreur/LivreurDeliveryDetails.java
@@ -0,0 +1,14 @@
+package com.lucdotdev.haraka.ui.home_livreur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LivreurDeliveryDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.livreur_delivery_details);
+    }
+}
